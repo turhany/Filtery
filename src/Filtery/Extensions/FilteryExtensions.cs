285bcf2dbb114dcab4e1aaf46730b6bd1840fdb0
Bahadır Yardım <bhdryrdm@gmail.com>
2021-11-06 02:14:01 +0300

    FilteryExtensions added for pagination.

---
+++ src/Filtery/Extensions/FilteryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Filtery.Models;
+using Filtery.Pager;
+
+namespace Filtery.Extensions
+{
+    public static class FilteryExtensions
+    {
+        public static Page GetPageInfo<T>(this FilteryResponse<T> response) => new Page()
+        {
+            PageNumber = response.PageNumber,
+            PageSize = response.PageSize,
+            TotalItemCount = response.TotalItemCount
+        };
+    }
+}
---
+++ src/Filtery/Pager/Page.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Filtery.Pager
+{
+    public class Page
+    {
+        public int TotalItemCount { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalPageCount => (int)Math.Ceiling(TotalItemCount / (double)PageSize);
+    }
+}
